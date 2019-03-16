dbcb62e1e94ddde94ca946e06479da5a73039a5f
qin.li <qin.li@samsung.com>
2019-03-16 11:56:28 +0800

    remove xtensa_add_resion in up_initialize as per supporting multiheap on public

--- os/arch/xtensa/src/xtensa/xtensa.h
+++ os/arch/xtensa/src/xtensa/xtensa.h
@@ -354,10 +354,7 @@ void weak_function xtensa_dma_initialize(void);
 /* Memory management */
 
 #if CONFIG_MM_REGIONS > 1
-void xtensa_add_region(void);
 void xtensa_up_addregion(void); //add region after spiram initialized
-#else
-#define xtensa_add_region()
 #endif
 
 /* Serial output */
